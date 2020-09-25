298d24cf85203620c5e9f3ea1e58e96cc7255c33
ishon19 <pathak.shreyans@gmail.com>
2020-09-25 13:33:01 +0530

    Initial Commit

---
+++ Intro/Earth.java
@@ -0,0 +1,13 @@
+package Intro;
+
+public class Earth {
+    public static void main(String s[]) {
+        Human man;
+        man = new Human();
+        man.age = 12;
+        man.eyeColor = "Black";
+        man.heightInInches = 34;
+        man.name = "Test";
+        man.speak();
+    }
+}
